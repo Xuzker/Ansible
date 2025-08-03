0a49472556f1bcdf8f1947f783ea1fc48fff35df
Xuzker <vyacheslav.gvozdikov@yandex.ru>
2025-08-03 16:11:20 +0300

    initial

---
+++ index.html.js
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Welcome to Ansible Managed Page</title>
+    <style>
+        body {
+            background-color: #f0f0f0;
+            font-family: Arial, sans-serif;
+            text-align: center;
+            padding-top: 50px;
+        }
+        h1 {
+            color: #007acc;
+        }
+    </style>
+</head>
+<body>
+    <h1>{{ custom_message }}</h1>
+    <p>This page is deployed automatically using <strong>Ansible</strong>.</p>
+</body>
+</html>
